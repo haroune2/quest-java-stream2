490f4be8669261c6b8d685ca8bfb6c0882edb4fe
haroune_b <125916355+harouneboutlookfr@users.noreply.github.com>
2024-02-07 22:08:32 +0000

    first commit

--- Shield.java
+++ Shield.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Shield {
 
@@ -12,8 +14,22 @@ public class Shield {
         names.add("Leopold Fitz");
         names.add("Jemma Simmons");
 
-        // TODO 1 : map names to agents list
         List<Agent> agents = new ArrayList<>();
+
+        // names
+        // .stream()
+        // .map(string -> agents.add(new Agent(string.split(" ")[0], string.split("
+        // ")[0])))
+        // .toList();
+        // Utilisation d'un Stream et de la méthode map avec une lambda pour transformer
+        // la liste de noms en une liste d'objets Agent
+        agents = names.stream()
+                .map(name -> {
+                    String[] nameParts = name.split(" ");
+                    return new Agent(nameParts[0], nameParts[1]);
+                })
+                .collect(Collectors.toList());
+
 
         showAgents(agents);
     }
